bb3a9728875d267d442d44e4fffca32547a5fd5e
grigory <grish.kov@mail.ru>
2024-04-04 17:45:16 +0700

    Deleted comments

--- my_test/my_test.srcs/sources_1/new/test.sv
+++ my_test/my_test.srcs/sources_1/new/test.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/04/2024 05:37:57 PM
-// Design Name: 
-// Module Name: test
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module test(
 
