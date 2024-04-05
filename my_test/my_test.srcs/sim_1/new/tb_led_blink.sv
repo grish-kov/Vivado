7f8f1aed72996628fd094a2b40ca9500589eff17
grigory <grish.kov@mail.ru>
2024-04-05 13:52:38 +0700

    Создание файлов для Лаб № 2

---
+++ my_test/my_test.srcs/sim_1/new/tb_led_blink.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/05/2024 01:50:17 PM
+// Design Name: 
+// Module Name: tb_led_blink
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_led_blink(
+
+    );
+endmodule
