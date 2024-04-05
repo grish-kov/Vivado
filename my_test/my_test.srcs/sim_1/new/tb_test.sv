1a070b80ef2856a35d8f802a21d1fbc6bd3c18ad
grigory <grish.kov@mail.ru>
2024-04-05 13:44:00 +0700

    Готовая Лаб № 1
    Я так и не понял для чего там if-else нужны, можно ведь без них реализовать

--- my_test/my_test.srcs/sim_1/new/tb_test.sv
+++ my_test/my_test.srcs/sim_1/new/tb_test.sv
@@ -5,21 +5,23 @@ module tb_test
     logic [3:0] x;
     logic [1:0] a;
     logic f;
-
+    
     test
     UUT(
         .x(x),
         .a(a),
         .f(f)
     );
-    
+       
+    always#(T_CLK) x[0] = !x[0];
+    always#(3 * T_CLK / 2) x[1] = !x[1];
+    always#(T_CLK - 5) x[2] = !x[2];
+    always#(2*2*2*2 * T_CLK / 20) x[3] = !x[3];
+    always#(300) a = a + 1;
     initial begin
-        
-        x[0] = T_CLK;
-        x[1] = (3 * T_CLK)/2;
-        x[2] = T_CLK - 5;
-        x[3] = (2*2*2*2 * T_CLK)/20;
-        
+        x='0;
+        a = 2'b00;
+       
     end
 
 endmodule
