b57618cd2c9dcedb3f192edc83ca34c011a0bf38
grigory <grish.kov@mail.ru>
2024-04-18 16:17:16 +0700

    Реализация конечного автомата в source без CRC

--- my_test/my_test.srcs/sources_1/new/lab4_source.sv
+++ my_test/my_test.srcs/sources_1/new/lab4_source.sv
@@ -1,15 +1,17 @@
 `timescale 1ns / 1ps
 module lab4_source #(
-    parameter N = 8
+    parameter N = 10,
+    parameter G_BYT = 1
+   
     ) (
     input i_clk, 
     input i_rst,
-    output m_axis_tready,
-	output m_axis_tvalid,
-	output m_axis_tlast,
-	output [N - 1:0] m_axis_tdata
+    output logic m_axis_tready,
+	output logic m_axis_tvalid = 0,
+	output logic m_axis_tlast,
+	output reg [N - 1:0] m_axis_tdata
 );
-    CRC u_crc(
+    CRC u_crc (
     
         .i_crc_a_clk_p  (i_clk),
         .i_crc_s_rst_p  (i_rst),
@@ -18,5 +20,109 @@ module lab4_source #(
     
     );
 
+    enum logic [7:0]{
+
+        S  /*  STOP      */ = 8'b0000000,
+        S0 /*  READY     */ = 8'b0000001,
+        S1 /*  INIT_H    */ = 8'b0000010,
+        S2 /*  INIT_L    */ = 8'b0000100,
+        S3 /*  PAYLOAD   */ = 8'b0001000,
+        S4 /*  CRC_PAUSE */ = 8'b0010000,
+        S5 /*  CRC       */ = 8'b0100000,
+        S6 /*  IDLE      */ = 8'b1000000
+        
+    } q_crnt_s;
+
+
+    if_axis #(.N(G_BYT)) m_axis();
+
+    reg [int'($ceil($clog2(N + 1))):0] q_cnt;
+    
+    assign m_axis_tready = 1;
+    
+    always_ff @(posedge i_clk) begin
+    
+        if (i_rst) begin
+
+            q_crnt_s <= S0;
+            q_cnt <= 0;
+
+        end
+            
+            case (q_crnt_s)
+                S0: begin
+                
+                    if(m_axis_tready)
+                        q_crnt_s <= S1;
+                    
+                end
+                S1: begin
+                
+                    if (m_axis_tready & !m_axis_tvalid) 
+                        m_axis_tvalid <= 1;
+                    
+                    m_axis.tdata <= 72;
+                    
+                    if(m_axis_tready & m_axis_tvalid) begin
+                        
+                        m_axis_tvalid <= 0; 
+                        q_crnt_s <= S2;
+
+                    end 
+                    
+                end
+                S2: begin
+                
+                    if (m_axis_tready & !m_axis_tvalid) 
+                        m_axis_tvalid <= 1;
+                    
+                    m_axis.tdata <= N;
+                    
+                    if(m_axis_tready & m_axis_tvalid) begin
+                        
+                        m_axis_tvalid <= 0;
+                        q_crnt_s <= S3;
+                        q_cnt <= 0;
+                            
+                    end
+                    
+                end
+                S3: begin
+
+                    if (m_axis_tready & !m_axis_tvalid) 
+                        m_axis_tvalid <= 1;
+                    
+                    if (q_cnt <= N) begin
+
+                        m_axis.tdata  <= 97 + q_cnt;
+                        q_cnt <= q_cnt + 1;
+
+                    end
+                    
+                    if (m_axis_tvalid & m_axis_tready & q_cnt == N) begin
+                        q_crnt_s <= S4;
+                        m_axis_tvalid <= 0;
+                        q_cnt <= 0;
+                    end
+
+                end
+                S4: begin
+
+                    q_crnt_s <= S5;
+                        
+                end
+                S5: begin
+                
+                    q_crnt_s <= S6;
+                    
+                end
+                S6: begin
+                
+                    q_crnt_s <= S0;
+                    
+                end
+                default: q_crnt_s <= S0;
+            endcase
+        end
 
 endmodule
--- my_test/my_test.srcs/sources_1/new/lab4_top.sv
+++ my_test/my_test.srcs/sources_1/new/lab4_top.sv
@@ -9,16 +9,8 @@ interface if_axis #(parameter int N = 1) ();
 endinterface
 
 module lab4_top #(
-    parameter G_BYT = 1,
-    parameter N = 10,
-    parameter S  /*  STOP      */ = 8'b0000000,
-    parameter S0 /*  READY     */ = 8'b0000001,
-    parameter S1 /*  INIT_H    */ = 8'b0000100,
-    parameter S2 /*  INIT_L    */ = 8'b0001000,
-    parameter S3 /*  PAYLOAD   */ = 8'b0001000,
-    parameter S4 /*  CRC_PAUSE */ = 8'b0010000,
-    parameter S5 /*  CRC       */ = 8'b0100000,
-    parameter S6 /*  IDLE      */ = 8'b1000000
+    parameter G_BYT = 1
+    
 ) (
     input       i_clk,
     input [2:0] i_rst
@@ -66,75 +58,4 @@ module lab4_top #(
         .s_axis_tlast       (slv_fifo.tlast)
         );
    
-    reg [7:0] q_crnt_s = S0;
-    reg [7:0] w_nxt_s;
-    
-    
-    always @(posedge i_clk) begin
-    
-        if (i_rst[0])
-            q_crnt_s <= S0;
-		else begin
-        	w_nxt_s = q_crnt_s;
-            case (q_crnt_s)
-                S0: begin
-                
-                    if(mst_fifo.tready) q_crnt_s = S1;
-                    else q_crnt_s = S0;
-                    
-                end
-                S1: begin
-                
-                    if(mst_fifo.tready & mst_fifo.tvalid) begin
-                        mst_fifo.tdata = 72;
-                        q_crnt_s = S2;    
-                    end 
-                    else q_crnt_s = S1;  
-                    
-                end
-                S2: begin
-                    
-                    if(mst_fifo.tready & mst_fifo.tvalid) begin
-                        mst_fifo.tdata = N;
-                        q_crnt_s = S3;
-                    end
-                    else q_crnt_s = S2;  
-                    
-                end
-                S3: begin
-
-                    if(mst_fifo.tready & mst_fifo.tvalid) begin
-                        for(int i = 2; i <= N; i ++) begin
-                            mst_fifo.tvalid = 1;
-                            mst_fifo.tlast  = (i == N);
-                            mst_fifo.tdata  = i;
-                            mst_fifo.tvalid = 0;
-                        end
-                        q_crnt_s = S4;
-                    end
-                    else q_crnt_s = S3;  
-
-                end
-                S4: begin
-
-                    q_crnt_s = S5;
-                        
-                end
-                S5: begin
-                
-                    q_crnt_s = S6;
-                    
-                end
-                S6: begin
-                
-                    q_crnt_s = S0;
-                    
-                end
-                default: q_crnt_s = S0;
-            endcase
-        end
-    end
-    
-    
-    
 endmodule
